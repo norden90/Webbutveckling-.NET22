bc1f6e0203d6cf47ca32f988d84f06d736b29922
norden90 <nordfeldt.andreas@gmail.com>
2023-01-30 11:36:36 +0100

    .

--- IntroJavaScript/script.js
+++ IntroJavaScript/script.js
@@ -1,3 +1,6 @@
-let letVar = 2;
-console.log(letVar)
-console.log("Hej svejs");
+const constVar = 69;
+let letVar = "Andreas";
+var varLet = 55;
+
+const presentationText = `hej ${constVar} ${letVar}`;
+console.log(presentationText);
